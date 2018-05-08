0dcacc774e45cb95338b06bc2206cb10e0e78a1c
Rajeev <purohitmrajeev@gmail.com>
2018-05-07 20:20:56 -0500

    adding port

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, postgres
 app.put('/image', (req, res) => {image.handleImage(req, res, postgres)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) })
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*
